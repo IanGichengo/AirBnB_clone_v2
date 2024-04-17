71cb25f15f28a840dd0226d087b0b5b297b475fe
IanGichengo <iangatimu6@gmail.com>
2024-04-17 15:42:41 -0400

    airbnb_v2

---
+++ setup_mysql_dev.sql
@@ -0,0 +1,9 @@
+-- Create the database hbnb_dev_db if it does not exist
+CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+
+-- Create or update the user hbnb_dev
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+
+-- Grant privileges to the user hbnb_dev
+GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
+GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
