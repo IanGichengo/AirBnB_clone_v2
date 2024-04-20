3756a6dd939013dbb86deb97b3bc45b93b990c2a
IanGichengo <iangatimu6@gmail.com>
2024-04-20 14:19:53 -0400

    airbnb_v2

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -4,30 +4,36 @@ import json
 
 
 class FileStorage:
-    """File storage class"""
-
-    __file_path = "file.json"
+    """This class manages storage of hbnb models in JSON format"""
+    __file_path = 'file.json'
     __objects = {}
 
-    def all(self):
-        """Returns the dictionary __objects"""
-        return type(self).__objects
+    def all(self, cls=None):
+        """Returns a dictionary of models currently in storage"""
+        if cls is None:
+            return type(self).__objects
+        else:
+            return {key: obj for key, obj in type(self).__objects.items()
+                    if isinstance(obj, cls)}
 
     def new(self, obj):
-        """Sets in __objects the obj with key <obj class name>.id"""
+        """Adds new object to storage dictionary"""
         key = "{}.{}".format(obj.__class__.__name__, obj.id)
         type(self).__objects[key] = obj
 
     def save(self):
-        """Serializes __objects to the JSON file"""
-        with open(type(self).__file_path, 'w', encoding='utf-8') as f:
-            obj_dict = {key: obj.to_dict() for key, obj in type(self).__objects.items()}
-            json.dump(obj_dict, f)
+        """Saves storage dictionary to file"""
+        with open(type(self).__file_path, 'w') as f:
+            temp = {}
+            temp.update(type(self).__objects)
+            for key, val in temp.items():
+                temp[key] = val.to_dict()
+            json.dump(temp, f)
 
     def reload(self):
-        """Deserializes the JSON file to __objects"""
+        """Loads storage dictionary from file"""
         try:
-            with open(type(self).__file_path, 'r', encoding='utf-8') as f:
+            with open(type(self).__file_path, 'r') as f:
                 obj_dict = json.load(f)
                 for key, val in obj_dict.items():
                     class_name = val['__class__']
@@ -36,29 +42,9 @@ class FileStorage:
         except FileNotFoundError:
             pass
 
-    @property
-    def amenities(self):
-        """Getter attribute for amenities"""
-        from models.amenity import Amenity
-
-        amenity_instances = []
-        for amenity_id in self.amenity_ids:
-            key = "Amenity." + amenity_id
-            amenity_instance = self.__objects.get(key)
-            if amenity_instance and isinstance(amenity_instance, Amenity):
-                amenity_instances.append(amenity_instance)
-        return amenity_instances
-
-    @amenities.setter
-    def amenities(self, obj):
-        """Setter attribute for amenities"""
-        if isinstance(obj, Amenity):
-            key = "{}.{}".format(obj.__class__.__name__, obj.id)
-            self.__objects[key] = obj
-
     def delete(self, obj=None):
-        """Deletes obj from __objects if it's inside """
+        """Deletes obj from __objects if it's inside"""
         if obj is not None:
-            key = obj.__class__.__name__ + '.' + obj.id
-        if key in self.__objects:
-            del self.__objects[key]
+            key = "{}.{}".format(obj.__class__.__name__, obj.id)
+            if key in self.__objects:
+                del self.__objects[key]
